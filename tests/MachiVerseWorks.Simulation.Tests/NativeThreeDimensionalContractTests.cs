9615049212a4bd9b81d8305c8f63ee90956d1c5d
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-08-29 22:31:25 +0900

    test: fix native 3D contract guard

--- tests/MachiVerseWorks.Simulation.Tests/NativeThreeDimensionalContractTests.cs
+++ tests/MachiVerseWorks.Simulation.Tests/NativeThreeDimensionalContractTests.cs
@@ -9,13 +9,14 @@ public sealed class NativeThreeDimensionalContractTests
     public void PublicGeometryContractHasNoTwoDimensionalCompatibilityTypesOrConstructors()
     {
         var assembly = typeof(WorldPoint).Assembly;
+        var legacyWorldRect = assembly.GetType("MachiVerseWorks.Simulation.WorldRect");
 
-        Assert.IsNull(assembly.GetType("MachiVerseWorks.Simulation.WorldRect"));
+        Assert.IsNull(legacyWorldRect);
         Assert.IsTrue(typeof(WorldPoint).GetConstructors().All(static constructor => constructor.GetParameters().Length == 3));
         Assert.IsTrue(typeof(WorldVector).GetConstructors().All(static constructor => constructor.GetParameters().Length == 3));
         Assert.IsTrue(typeof(SpatialCell).GetConstructors().All(static constructor => constructor.GetParameters().Length == 3));
         Assert.IsTrue(typeof(SimulationWorld).GetMethods().Where(static method => method.Name is "CreateAgents" or "CreateSnapshot")
             .SelectMany(static method => method.GetParameters())
-            .All(static parameter => parameter.ParameterType != assembly.GetType("MachiVerseWorks.Simulation.WorldRect")));
+            .All(parameter => parameter.ParameterType != legacyWorldRect));
     }
 }
