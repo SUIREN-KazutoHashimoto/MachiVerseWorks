c9fd823808537a541c87f44f25d55d15c0c6123a
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-08-30 13:07:01 +0900

    feat: add Vehicle Save format version

--- src/MachiVerseWorks.Persistence/SaveFormatVersion.cs
+++ src/MachiVerseWorks.Persistence/SaveFormatVersion.cs
@@ -5,5 +5,6 @@ public static class SaveFormatVersion
     public const int BuildingPoi = 3;
     public const int RoadNetwork = 4;
     public const int Pedestrian = 5;
-    public const int Current = Pedestrian;
+    public const int Vehicle = 6;
+    public const int Current = Vehicle;
 }
