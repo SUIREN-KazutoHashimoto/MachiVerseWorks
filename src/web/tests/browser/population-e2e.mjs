2e6702e8b7063dac7a42cc12f1c79b0c5c11c8a8
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-01 08:01:23 +0900

    Update population E2E for Protocol 2.11

--- src/web/tests/browser/population-e2e.mjs
+++ src/web/tests/browser/population-e2e.mjs
@@ -47,7 +47,7 @@ ui.onClearPersonInspection(() => connection.clearPersonInspection());
 try {
   connection.connect();
   await waitUntil(() => connectionState === 'connected', 'Protocol connection');
-  assert(protocolVersion?.major === 2 && protocolVersion?.minor === 10, 'current Browser connection negotiates Protocol 2.10');
+  assert(protocolVersion?.major === 2 && protocolVersion?.minor === 11, 'current Browser connection negotiates Protocol 2.11');
   await waitUntil(() => populationStatistics !== null, 'PopulationStatistics');
   assert(populationStatistics.householdCount === 1, 'PopulationStatistics contains one Household');
   assert(populationStatistics.personCount === 1, 'PopulationStatistics contains one Person');
