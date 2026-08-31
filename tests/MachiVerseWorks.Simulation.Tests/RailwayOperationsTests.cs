5d7e719a56274220a3fad2025c29221247c8a8cf
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-08-31 15:45:11 +0900

    test: Railway Route failure atomicity fixtureを修正

--- tests/MachiVerseWorks.Simulation.Tests/RailwayOperationsTests.cs
+++ tests/MachiVerseWorks.Simulation.Tests/RailwayOperationsTests.cs
@@ -25,19 +25,22 @@ public sealed class RailwayOperationsTests
         var a = world.CreateTrackNode(new WorldPoint(0d, 0d, 0d));
         var b = world.CreateTrackNode(new WorldPoint(10d, 0d, 0d), TrackNodeKind.Junction);
         var c = world.CreateTrackNode(new WorldPoint(20d, 0d, 0d));
+        var d = world.CreateTrackNode(new WorldPoint(0d, 20d, 0d));
+        var e = world.CreateTrackNode(new WorldPoint(10d, 20d, 0d));
         var first = world.CreateTrackSegment(a, b);
         var second = world.CreateTrackSegment(b, c);
+        var disconnected = world.CreateTrackSegment(d, e);
+        world.CreateTrackConnection(first, second, b);
         var expectedNextId = world.CreateCheckpoint().NextRailwayRouteId;
 
         Assert.ThrowsExactly<ArgumentException>(() => world.CreateRailwayRoute([new TrackSegmentId(ulong.MaxValue)]));
-        Assert.ThrowsExactly<ArgumentException>(() => world.CreateRailwayRoute([first, second]));
+        Assert.ThrowsExactly<ArgumentException>(() => world.CreateRailwayRoute([first, disconnected]));
         Assert.ThrowsExactly<ArgumentException>(() => world.CreateRailwayRoute([first, first]));
 
         var failed = world.CreateCheckpoint();
         Assert.AreEqual(expectedNextId, failed.NextRailwayRouteId);
         Assert.AreEqual(0, failed.RailwayRoutes!.Count);
 
-        world.CreateTrackConnection(first, second, b);
         var created = world.CreateRailwayRoute([first, second]);
         Assert.AreEqual(expectedNextId, created.Value);
         Assert.AreEqual(expectedNextId + 1, world.CreateCheckpoint().NextRailwayRouteId);
