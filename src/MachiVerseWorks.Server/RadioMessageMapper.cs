4a00e8e2443fec1f21149390330acf8bbf9c678a
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-01 22:16:32 +0900

    feat(server): map full radio entity snapshots

--- src/MachiVerseWorks.Server/RadioMessageMapper.cs
+++ src/MachiVerseWorks.Server/RadioMessageMapper.cs
@@ -17,6 +17,20 @@ internal static class RadioMessageMapper
         var sites = snapshot.Sites.Take(MaximumDebugEntries)
             .Select(static item => new ProtocolRadioSite(item.Id.Value, (ProtocolRadioSiteKind)item.Kind, item.Position.X, item.Position.Y, item.Position.Z, item.AntennaGainDb, item.AntennaHeightMeters, item.IsInService)).ToArray();
         var siteIds = sites.Select(static item => item.SiteId).ToHashSet();
+        var antennas = (snapshot.Antennas ?? []).Where(item => siteIds.Contains(item.SiteId.Value)).Take(MaximumDebugEntries)
+            .Select(static item => new ProtocolRadioAntenna(
+                item.Id.Value, item.SiteId.Value,
+                item.PositionOffset.X, item.PositionOffset.Y, item.PositionOffset.Z,
+                item.Orientation.X, item.Orientation.Y, item.Orientation.Z,
+                item.GainDb, (ProtocolRadioAntennaPatternKind)item.PatternKind, item.BeamwidthDegrees, item.FrontToBackRatioDb, item.IsInService)).ToArray();
+        var antennaIds = antennas.Select(static item => item.AntennaId).ToHashSet();
+        var transmitters = (snapshot.Transmitters ?? []).Where(item => siteIds.Contains(item.SiteId.Value) && antennaIds.Contains(item.AntennaId.Value)).Take(MaximumDebugEntries)
+            .Select(static item => new ProtocolRadioTransmitter(item.Id.Value, item.SiteId.Value, item.AntennaId.Value, item.MaximumTransmitPowerDbm, item.IsInService, item.IsOperational)).ToArray();
+        var transmitterIds = transmitters.Select(static item => item.TransmitterId).ToHashSet();
+        var receivers = (snapshot.Receivers ?? []).Where(item => siteIds.Contains(item.SiteId.Value) && antennaIds.Contains(item.AntennaId.Value)).Take(MaximumDebugEntries)
+            .Select(static item => new ProtocolRadioReceiver(item.Id.Value, item.SiteId.Value, item.AntennaId.Value, item.MinimumFrequencyMegahertz, item.MaximumFrequencyMegahertz, item.SensitivityDbm, item.IsInService, item.IsOperational)).ToArray();
+        var emissions = (snapshot.Emissions ?? []).Where(item => transmitterIds.Contains(item.TransmitterId.Value)).Take(MaximumDebugEntries)
+            .Select(static item => new ProtocolRadioEmission(item.Id.Value, item.TransmitterId.Value, item.ChannelId.Value, item.CenterFrequencyMegahertz, item.BandwidthMegahertz, item.TransmitPowerDbm, item.Utilization, item.IsInService, item.IsOperational)).ToArray();
         var links = snapshot.Links.Where(item => siteIds.Contains(item.FromSiteId.Value) && siteIds.Contains(item.ToSiteId.Value)).Take(MaximumDebugEntries)
             .Select(static item => new ProtocolRadioLink(item.Id.Value, item.FromSiteId.Value, item.ToSiteId.Value, item.FrequencyBlockId.Value, item.DistanceMeters, item.PathLossDb, item.ReceivedPowerDbm, item.InterferenceDbm, item.SinrDb, item.Utilization, (ProtocolRadioLinkState)item.State, item.IsInService)).ToArray();
         var serviceAreas = snapshot.ServiceAreas.Where(item => siteIds.Contains(item.SiteId.Value)).Take(MaximumDebugEntries)
@@ -28,7 +42,15 @@ internal static class RadioMessageMapper
         var conflicts = snapshot.Conflicts.Take(MaximumDebugEntries)
             .Select(static item => new ProtocolSpectrumConflict(item.FirstBlockId.Value, item.SecondBlockId.Value, item.FirstSiteId.Value, item.SecondSiteId.Value, item.Reason)).ToArray();
         return (
-            new RadioSnapshotMessage(statistics, Array.AsReadOnly(sites), Array.AsReadOnly(links), Array.AsReadOnly(serviceAreas)),
+            new RadioSnapshotMessage(
+                statistics,
+                Array.AsReadOnly(sites),
+                Array.AsReadOnly(antennas),
+                Array.AsReadOnly(transmitters),
+                Array.AsReadOnly(receivers),
+                Array.AsReadOnly(emissions),
+                Array.AsReadOnly(links),
+                Array.AsReadOnly(serviceAreas)),
             new SpectrumSnapshotMessage(s.TickCount, Array.AsReadOnly(bands), Array.AsReadOnly(blocks), Array.AsReadOnly(conflicts)));
     }
 }
