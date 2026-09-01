372c689610eb45b7556cf303e442c4fa57de50b9
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-01 10:06:36 +0900

    Phase 23: update protocol version tests for 2.12

--- tests/MachiVerseWorks.Protocol.Tests/ProtocolVersionTests.cs
+++ tests/MachiVerseWorks.Protocol.Tests/ProtocolVersionTests.cs
@@ -6,24 +6,26 @@ namespace MachiVerseWorks.Protocol.Tests;
 public sealed class ProtocolVersionTests
 {
     [TestMethod]
-    public void CurrentProtocolIs211AndAdvertisesLogistics()
+    public void CurrentProtocolIs212AndAdvertisesPower()
     {
-        Assert.AreEqual(new ProtocolVersion(2, 11), ProtocolVersion.Current);
+        Assert.AreEqual(new ProtocolVersion(2, 12), ProtocolVersion.Current);
         Assert.IsTrue(ProtocolVersion.Current.SupportsMultimodalTransit);
         Assert.IsTrue(ProtocolVersion.Current.SupportsPersonInspectionClear);
         Assert.IsTrue(ProtocolVersion.Current.SupportsEconomy);
         Assert.IsTrue(ProtocolVersion.Current.SupportsLogistics);
+        Assert.IsTrue(ProtocolVersion.Current.SupportsPower);
         Assert.IsFalse(new ProtocolVersion(2, 7).SupportsMultimodalTransit);
         Assert.IsFalse(new ProtocolVersion(2, 8).SupportsPersonInspectionClear);
         Assert.IsFalse(new ProtocolVersion(2, 9).SupportsEconomy);
         Assert.IsFalse(new ProtocolVersion(2, 10).SupportsLogistics);
+        Assert.IsFalse(new ProtocolVersion(2, 11).SupportsPower);
     }
 
     [TestMethod]
     public void NegotiationKeepsAcceptedRequestedMinorAsTheConnectionVersion()
     {
-        var supported = new ProtocolVersion(2, 11);
-        var requested = new ProtocolVersion(2, 10);
+        var supported = new ProtocolVersion(2, 12);
+        var requested = new ProtocolVersion(2, 11);
 
         var accepted = supported.TryNegotiate(requested, out var negotiated);
 
@@ -34,10 +36,10 @@ public sealed class ProtocolVersionTests
     [TestMethod]
     public void NegotiationRejectsNewerMinorAndDifferentMajor()
     {
-        var supported = new ProtocolVersion(2, 11);
+        var supported = new ProtocolVersion(2, 12);
 
-        Assert.IsFalse(supported.TryNegotiate(new ProtocolVersion(2, 12), out _));
-        Assert.IsFalse(supported.TryNegotiate(new ProtocolVersion(1, 11), out _));
+        Assert.IsFalse(supported.TryNegotiate(new ProtocolVersion(2, 13), out _));
+        Assert.IsFalse(supported.TryNegotiate(new ProtocolVersion(1, 12), out _));
         Assert.IsFalse(supported.TryNegotiate(new ProtocolVersion(3, 0), out _));
     }
 }
