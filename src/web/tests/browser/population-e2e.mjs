12cddbf2ac067a60e574e1e3612502b3a0d4213c
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-01 13:50:34 +0900

    test: bump Population browser E2E to Protocol 2.14

--- src/web/tests/browser/population-e2e.mjs
+++ src/web/tests/browser/population-e2e.mjs
@@ -47,7 +47,7 @@ ui.onClearPersonInspection(() => connection.clearPersonInspection());
 try {
   connection.connect();
   await waitUntil(() => connectionState === 'connected', 'Protocol connection');
-  assert(protocolVersion?.major === 2 && protocolVersion?.minor === 13, 'current Browser connection negotiates Protocol 2.13');
+  assert(protocolVersion?.major === 2 && protocolVersion?.minor === 14, 'current Browser connection negotiates Protocol 2.14');
   await waitUntil(() => populationStatistics !== null, 'PopulationStatistics');
   assert(populationStatistics.householdCount === 1, 'PopulationStatistics contains one Household');
   assert(populationStatistics.personCount === 1, 'PopulationStatistics contains one Person');
