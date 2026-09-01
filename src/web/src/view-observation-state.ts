efc93b3a4a19aed819fe7b5d82a1d985811fb5eb
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-02 08:41:55 +0900

    refactor(view): hide writable stores behind read-only contracts

--- src/web/src/view-observation-state.ts
+++ src/web/src/view-observation-state.ts
@@ -15,46 +15,52 @@ export interface ReadonlyViewObservationState {
 
 /** Single writable ingress for observation messages used by the View. */
 export class ViewObservationState implements ReadonlyViewObservationState {
-  public readonly entities = new EntityStore();
-  public readonly pedestrians = new PedestrianStore();
-  public readonly vehicles = new VehicleStore();
-  public readonly intersections = new IntersectionControlStore();
-  public readonly roadNetwork = new RoadNetworkStore();
+  private readonly entityStore = new EntityStore();
+  private readonly pedestrianStore = new PedestrianStore();
+  private readonly vehicleStore = new VehicleStore();
+  private readonly intersectionStore = new IntersectionControlStore();
+  private readonly roadNetworkStore = new RoadNetworkStore();
+
+  public readonly entities: ReadonlyEntityStore = this.entityStore;
+  public readonly pedestrians: ReadonlyPedestrianStore = this.pedestrianStore;
+  public readonly vehicles: ReadonlyVehicleStore = this.vehicleStore;
+  public readonly intersections: ReadonlyIntersectionControlStore = this.intersectionStore;
+  public readonly roadNetwork: ReadonlyRoadNetworkStore = this.roadNetworkStore;
 
   public apply(message: ProtocolMessage | TrafficProtocolMessage, receivedAt = performance.now()): boolean {
     switch (message.type) {
       case MessageType.AgentSpawn:
-        this.entities.spawn(message, receivedAt);
+        this.entityStore.spawn(message, receivedAt);
         return true;
       case MessageType.AgentUpdate:
-        if (!this.entities.update(message, receivedAt)) this.entities.spawn(message, receivedAt);
+        if (!this.entityStore.update(message, receivedAt)) this.entityStore.spawn(message, receivedAt);
         return true;
       case MessageType.AgentRemove:
-        this.entities.remove(message.agentId);
+        this.entityStore.remove(message.agentId);
         return true;
       case MessageType.PedestrianSpawn:
-        this.pedestrians.spawn(message, receivedAt);
+        this.pedestrianStore.spawn(message, receivedAt);
         return true;
       case MessageType.PedestrianUpdate:
-        if (!this.pedestrians.update(message, receivedAt)) this.pedestrians.spawn(message, receivedAt);
+        if (!this.pedestrianStore.update(message, receivedAt)) this.pedestrianStore.spawn(message, receivedAt);
         return true;
       case MessageType.PedestrianRemove:
-        this.pedestrians.remove(message.pedestrianId);
+        this.pedestrianStore.remove(message.pedestrianId);
         return true;
       case MessageType.RoadNetworkSnapshot:
-        this.roadNetwork.replace(message);
+        this.roadNetworkStore.replace(message);
         return true;
       case TrafficMessageType.VehicleSpawn:
-        this.vehicles.spawn(message, receivedAt);
+        this.vehicleStore.spawn(message, receivedAt);
         return true;
       case TrafficMessageType.VehicleUpdate:
-        if (!this.vehicles.update(message, receivedAt)) this.vehicles.spawn(message, receivedAt);
+        if (!this.vehicleStore.update(message, receivedAt)) this.vehicleStore.spawn(message, receivedAt);
         return true;
       case TrafficMessageType.VehicleRemove:
-        this.vehicles.remove(message.vehicleId);
+        this.vehicleStore.remove(message.vehicleId);
         return true;
       case TrafficMessageType.IntersectionControlSnapshot:
-        this.intersections.apply(message, receivedAt);
+        this.intersectionStore.apply(message, receivedAt);
         return true;
       default:
         return false;
@@ -63,10 +69,10 @@ export class ViewObservationState implements ReadonlyViewObservationState {
 
   /** Clears connection-local authoritative observations and all visual interpolation history. */
   public resetConnectionState(): void {
-    this.entities.clear();
-    this.pedestrians.clear();
-    this.vehicles.clear();
-    this.intersections.clear();
-    this.roadNetwork.clear();
+    this.entityStore.clear();
+    this.pedestrianStore.clear();
+    this.vehicleStore.clear();
+    this.intersectionStore.clear();
+    this.roadNetworkStore.clear();
   }
 }
