fdac0bf3578592dd248addb97d779d548fe3875b
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-01 19:55:14 +0900

    test: fix ASP.NET Core proxy test imports

--- tests/MachiVerseWorks.Server.Tests/HttpsReverseProxyTestHost.cs
+++ tests/MachiVerseWorks.Server.Tests/HttpsReverseProxyTestHost.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
