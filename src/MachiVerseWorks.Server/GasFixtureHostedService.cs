3b5c5a9796d581a14c4e1801aafbb355f1bc9558
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-01 13:37:50 +0900

    test: make delivered Gas stockout deterministic

--- src/MachiVerseWorks.Server/GasFixtureHostedService.cs
+++ src/MachiVerseWorks.Server/GasFixtureHostedService.cs
@@ -27,20 +27,20 @@ internal sealed class GasFixtureHostedService(SimulationRuntime simulation, ICon
     private void Seed(SimulationWorld world)
     {
         var supplierBuilding = world.CreateBuilding(new WorldVolume(0d, 20d, 0d, 10d, 30d, 8d), BuildingKind.Industrial);
-        var consumerBuilding = world.CreateBuilding(new WorldVolume(40d, 20d, 0d, 50d, 30d, 8d), BuildingKind.Commercial);
+        var consumerBuilding = world.CreateBuilding(new WorldVolume(395d, 20d, 0d, 405d, 30d, 8d), BuildingKind.Commercial);
         var sourceNode = world.CreateGasNode(new WorldPoint(5d, 15d, 0d), GasNodeKind.Source);
-        var serviceNode = world.CreateGasNode(new WorldPoint(45d, 25d, 0d), GasNodeKind.Service);
+        var serviceNode = world.CreateGasNode(new WorldPoint(400d, 25d, 0d), GasNodeKind.Service);
         _pipelineId = world.CreateGasPipeline(sourceNode, serviceNode, 30d);
         world.CreateGasSource(sourceNode, 30d);
         world.CreateGasStorage(sourceNode, 100d, 40d, 10d);
         world.CreatePipedGasServicePoint(serviceNode, 12d, buildingId: consumerBuilding);
 
         var roadStart = world.CreateRoadNode(new WorldPoint(5d, 35d, 0d));
-        var roadEnd = world.CreateRoadNode(new WorldPoint(45d, 35d, 0d));
+        var roadEnd = world.CreateRoadNode(new WorldPoint(400d, 35d, 0d));
         var segment = world.CreateRoadSegment(roadStart, roadEnd, RoadKind.Local);
         world.CreateLane(segment, LaneDirection.Forward, 0, speedLimitMetersPerSecond: 12d);
-        var supplierAccess = world.CreateRoadAccessPoint(segment, 0.05d, supplierBuilding, mode: RoadAccessMode.Motor);
-        var consumerAccess = world.CreateRoadAccessPoint(segment, 0.95d, consumerBuilding, mode: RoadAccessMode.Motor);
+        var supplierAccess = world.CreateRoadAccessPoint(segment, 0.02d, supplierBuilding, mode: RoadAccessMode.Motor);
+        var consumerAccess = world.CreateRoadAccessPoint(segment, 0.98d, consumerBuilding, mode: RoadAccessMode.Motor);
         var supplierCompany = world.CreateCompany(IndustrySector.Transport, 100_000, 0d);
         var consumerCompany = world.CreateCompany(IndustrySector.Services, 100_000, 0d);
         var supplier = world.CreateEstablishment(supplierCompany, buildingId: supplierBuilding);
