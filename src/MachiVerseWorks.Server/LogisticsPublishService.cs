ab2a688672e07a690c7b25910a7d8e83f90a336c
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-02 14:56:09 +0900

    Align logistics delivery call with token convention

--- src/MachiVerseWorks.Server/LogisticsPublishService.cs
+++ src/MachiVerseWorks.Server/LogisticsPublishService.cs
@@ -27,11 +27,11 @@ internal sealed class LogisticsPublishService(
                 {
                     _ = deliveryCoordinator.TrySchedule(
                         connection,
-                        stoppingToken,
                         async sendCancellation =>
                         {
                             _ = await connection.SendAsync(message, connection.NegotiatedVersion, sendCancellation);
-                        });
+                        },
+                        stoppingToken);
                 }
             }
         }
