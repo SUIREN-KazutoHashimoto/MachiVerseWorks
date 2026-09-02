b68b7d8e2b8f3311fe4a9d45e1466fc1de98f060
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-02 09:54:59 +0900

    test: allow phase29 large-world subscription in e2e

--- scripts/run-phase29-e2e.sh
+++ scripts/run-phase29-e2e.sh
@@ -41,7 +41,7 @@ wait_http() {
 
 start_server() {
   local suffix="$1"
-  env Server__Port="$SERVER_PORT" Simulation__TickRate=30 Simulation__Seed=29027 Simulation__SpatialCellSize=4096 Server__SnapshotRate=2 Server__AllowedWebSocketOrigins="http://127.0.0.1:$WEB_PORT" Simulation__InitialAgentCount=0 dotnet run --project "$ROOT_DIR/src/MachiVerseWorks.Server/MachiVerseWorks.Server.csproj" --configuration Release --no-build >"$ARTIFACT_DIR/server-$suffix.log" 2>&1 & SERVER_PID=$!
+  env Server__Port="$SERVER_PORT" Simulation__TickRate=30 Simulation__Seed=29027 Simulation__SpatialCellSize=4096 Server__SnapshotRate=2 Server__MaximumSubscriptionCellCount=524288 Server__AllowedWebSocketOrigins="http://127.0.0.1:$WEB_PORT" Simulation__InitialAgentCount=0 dotnet run --project "$ROOT_DIR/src/MachiVerseWorks.Server/MachiVerseWorks.Server.csproj" --configuration Release --no-build >"$ARTIFACT_DIR/server-$suffix.log" 2>&1 & SERVER_PID=$!
   wait_http "http://127.0.0.1:$SERVER_PORT/health"
 }
 
