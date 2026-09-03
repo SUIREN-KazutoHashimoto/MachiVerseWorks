22b9925dd7857140d38b9a467c8e817e491461e2
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-03 16:11:13 +0900

    test(web): update current protocol expectation to 2.23

--- src/web/tests/radio-protocol.test.mjs
+++ src/web/tests/radio-protocol.test.mjs
@@ -4,8 +4,8 @@ import { WEB_CURRENT_PROTOCOL_VERSION } from '../src/person-inspection-protocol.
 import { RADIO_SNAPSHOT_MESSAGE_TYPE, SPECTRUM_SNAPSHOT_MESSAGE_TYPE, decodeRadioFrame, isRadioFrame } from '../src/radio-protocol.ts';
 import { PROTOCOL_HEADER_SIZE, PROTOCOL_MAGIC } from '../src/protocol.ts';
 
-test('web negotiates current protocol 2.20 while retaining radio support', () => {
-  assert.deepEqual(WEB_CURRENT_PROTOCOL_VERSION, { major: 2, minor: 20 });
+test('web negotiates current protocol 2.23 while retaining radio support', () => {
+  assert.deepEqual(WEB_CURRENT_PROTOCOL_VERSION, { major: 2, minor: 23 });
 });
 
 test('decodes empty radio snapshot frame', () => {
