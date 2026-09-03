9b0064488bf8f3c24c837aff87847d1c10307bfa
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-03 13:29:46 +0900

    test(environment): assert generated feature availability

--- tests/MachiVerseWorks.Simulation.Tests/WorldEnvironmentObservationContractTests.cs
+++ tests/MachiVerseWorks.Simulation.Tests/WorldEnvironmentObservationContractTests.cs
@@ -19,7 +19,9 @@ public sealed class WorldEnvironmentObservationContractTests
             annualPrecipitationMillimeters: 950d);
         var world = new SimulationWorld(new SimulationConfig(worldEnvironment: config));
         var volume = new WorldVolume(-500_000d, -500_000d, -10_000d, 500_000d, 500_000d, 10_000d);
-        var feature = world.GetGeographicFeatures(volume, 64)[0];
+        var features = world.GetGeographicFeatures(volume, 64);
+        Assert.IsTrue(features.Count > 0);
+        var feature = features[0];
         var before = world.CreateCheckpoint();
 
         var first = world.CreateNaturalToponym(feature);
