7bb8221e2092c6a278d7609273d20db51d18f9f7
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-04 22:25:45 +0900

    fix: validate transient mobility before topology changes

--- src/MachiVerseWorks.Simulation/SimulationWorld.InitialMobility.Lifecycle.cs
+++ src/MachiVerseWorks.Simulation/SimulationWorld.InitialMobility.Lifecycle.cs
@@ -2,6 +2,32 @@ namespace MachiVerseWorks.Simulation;
 
 public sealed partial class SimulationWorld
 {
+    private int CountStoredInitialPedestrians()
+    {
+        var count = 0;
+        foreach (var pedestrianId in _initialMobilityPedestrianIds)
+        {
+            if (_pedestrians.TryGetSnapshot(pedestrianId, Time.TickCount, out _)) count++;
+        }
+        return count;
+    }
+
+    private int CountStoredInitialVehicles()
+    {
+        var count = 0;
+        foreach (var vehicleId in _initialMobilityVehicleIds)
+        {
+            if (_vehicles.TryGetSnapshot(vehicleId, Time.TickCount, out _)) count++;
+        }
+        return count;
+    }
+
+    private void EnsurePedestrianNetworkMutable()
+    {
+        if (_pedestrians.Count > CountStoredInitialPedestrians())
+            throw new InvalidOperationException("Road topology cannot be changed while stored Pedestrians reference derived routes. Remove them before mutating the walk network.");
+    }
+
     private void RetireInitialVehiclesForRoadTopologyMutation()
     {
         foreach (var vehicleId in _initialMobilityVehicleIds.ToArray())
@@ -10,4 +36,6 @@ public sealed partial class SimulationWorld
             _initialMobilityVehicleIds.Remove(vehicleId);
         }
     }
+
+    private void RetireTransientInitialMobilityForCheckpoint() => RetireInitialMobilityForRoadTopologyMutation();
 }
